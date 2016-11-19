08c9ee6999a4c063b942748b09900559b05255e0
Andrew Dryga <andrew@dryga.com>
2016-11-19 17:46:52 +0200

    And even happier

--- config/.credo.exs
+++ config/.credo.exs
@@ -9,7 +9,7 @@
         {Credo.Check.Design.TagTODO, exit_status: 0},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 120},
         {Credo.Check.Refactor.FunctionArity, priority: :low, max_airty: 8},
-        {Credo.Check.Warning.UnusedKeywordOperation, exit_status: 0}
+        {Credo.Check.Warning.UnusedEnumOperation, false}
       ]
     }
   ]
