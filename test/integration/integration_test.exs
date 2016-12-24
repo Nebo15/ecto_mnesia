da35008a1d94826f61215f12ead039cd14a1eec0
Andrew Dryga <andrew@dryga.com>
2016-12-24 19:50:26 +0200

    Prepare integration tests

---
+++ test/integration/integration_test.exs
@@ -0,0 +1,19 @@
+# Passed tests
+Code.require_file "../../deps/ecto/integration_test/cases/migrator.exs", __DIR__
+
+# In progress
+# Code.require_file "../../deps/ecto/integration_test/cases/repo.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/cases/assoc.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/cases/interval.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/cases/preload.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/cases/type.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/sql/alter.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/sql/migration.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/sql/transaction.exs", __DIR__
+
+# TBD
+# Code.require_file "../../deps/ecto/integration_test/sql/lock.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/cases/joins.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/sql/sandbox.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/sql/sql.exs", __DIR__
+
