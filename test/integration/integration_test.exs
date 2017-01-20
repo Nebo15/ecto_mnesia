b753090b63b7af94bcb3d6d342dfaf778aeb0183
Andrew Dryga <andrew@dryga.com>
2017-01-20 16:40:39 +0200

    Run integration tests

--- test/integration/integration_test.exs
+++ test/integration/integration_test.exs
@@ -2,12 +2,12 @@
 Code.require_file "../../deps/ecto/integration_test/cases/migrator.exs", __DIR__
 
 # In progress
-# Code.require_file "../../deps/ecto/integration_test/cases/repo.exs", __DIR__
+Code.require_file "../../deps/ecto/integration_test/cases/repo.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/cases/type.exs", __DIR__
+# Code.require_file "../../deps/ecto/integration_test/sql/alter.exs", __DIR__
 # Code.require_file "../../deps/ecto/integration_test/cases/assoc.exs", __DIR__
 # Code.require_file "../../deps/ecto/integration_test/cases/interval.exs", __DIR__
 # Code.require_file "../../deps/ecto/integration_test/cases/preload.exs", __DIR__
-# Code.require_file "../../deps/ecto/integration_test/cases/type.exs", __DIR__
-# Code.require_file "../../deps/ecto/integration_test/sql/alter.exs", __DIR__
 # Code.require_file "../../deps/ecto/integration_test/sql/migration.exs", __DIR__
 # Code.require_file "../../deps/ecto/integration_test/sql/transaction.exs", __DIR__
 
